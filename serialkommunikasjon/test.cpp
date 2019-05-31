0a221618d494fc552d0cd25c55989aa4ade63a6a
Jørgen Navjord <navjordj@gmail.com>
2019-05-31 17:00:15 +0200

    Lagt til arduinoprogram

--- serialkommunikasjon/test.cpp
+++ serialkommunikasjon/test.cpp
@@ -46,7 +46,7 @@ int main()
     // tty.c_oflag &= ~OXTABS; // Prevent conversion of tabs to spaces (NOT PRESENT ON LINUX)
     // tty.c_oflag &= ~ONOEOT; // Prevent removal of C-d chars (0x004) in output (NOT PRESENT ON LINUX)
 
-    tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+    tty.c_cc[VTIME] = 0;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
     tty.c_cc[VMIN] = 0;
 
     // Set in/out baud rate to be 9600
---
+++ serialkommunikasjon/test.ino
@@ -0,0 +1,14 @@
+String incomingString = "";
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+    incomingString = Serial.readString();
+    Serial.println(incomingString);
+  }
+}
